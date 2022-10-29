43302a8c2dc8f3c0395b7754279313579472b34d
VarsharaneeAR <100801262+VarsharaneeAR@users.noreply.github.com>
2022-10-29 17:15:05 +0400

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+     
+     <div className="container">
      <h1>Weather Application </h1>
-
      <footer>
       This application was coded by <a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank"> VarsharaneeAR  </a> and is {""}
      <a href="https://github.com/VarsharaneeAR/react-weather-forecast-app" target="_blank">open-sourced on Github</a>
       
     </footer>
+    </div>
     </div>
   );
 }
