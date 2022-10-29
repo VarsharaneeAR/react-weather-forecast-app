72bb9ceddbabf791920187c708ee9b9ef36ef17f
VarsharaneeAR <100801262+VarsharaneeAR@users.noreply.github.com>
2022-10-29 16:01:53 +0400

    added links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather Application 
+     <h1>Weather Application </h1>
+
+     <footer>
+      This application was coded by <a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank"> VarsharaneeAR  </a> and is {""}
+     <a href="https://github.com/VarsharaneeAR/react-weather-forecast-app" target="_blank">open-sourced on Github</a>
+      
+    </footer>
     </div>
   );
 }
